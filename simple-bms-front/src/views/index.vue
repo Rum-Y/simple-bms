2d142db7c4f9ae2dc38b6cc009d1f73ecccc266b
Rum121003 <13570284+rum121003@user.noreply.gitee.com>
2025-05-08 14:26:04 +0800

    已去除用户部门关系，用户岗位关系，角色部门关系，注释DataScope注解的引用让对应切面不生效

---
+++ simple-bms-front/src/views/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="app-container home">
+    <h1>首页</h1>
+  </div>
+</template>
+
+<script setup name="Index">
+// const version = ref('3.8.4')
+
+// function goTarget(url) {
+//   window.open(url, '__blank')
+// }
+</script>
+
+<style scoped lang="scss">
+
+</style>
+
